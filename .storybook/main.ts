d3a0f92b367ecb2dfb3e747585072dfcd5ce7b55
Taytay <taylancetin06@gmail.com>
2025-08-06 22:42:16 +0300

    fix storybook build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,43 +1,45 @@
-import { dirname, join } from "node:path";
-import type { StorybookConfig } from "storybook-react-rsbuild";
+import { dirname, join } from 'node:path';
+import type { StorybookConfig } from 'storybook-react-rsbuild';
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 const reactIslandsUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3001"
-    : process.env.VERCEL_ENV === "production"
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001'
+    : process.env.VERCEL_ENV === 'production'
       ? process.env.VERCEL_PROJECT_PRODUCTION_URL
         ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/mf`
-        : ""
-      : process.env.VERCEL_ENV === "preview"
+        : ''
+      : process.env.VERCEL_ENV === 'preview'
         ? process.env.VERCEL_URL
           ? `https://${process.env.VERCEL_URL}/mf`
-          : ""
-        : "";
+          : ''
+        : '';
+
+console.log('Module federation base set as: ', reactIslandsUrl);
 
 const config: StorybookConfig = {
   stories: [
-    "../src/stories/**/*.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../src/stories/**/*.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   framework: {
-    name: getAbsolutePath("storybook-react-rsbuild"),
+    name: getAbsolutePath('storybook-react-rsbuild'),
     options: {},
   },
   addons: [
     {
-      name: getAbsolutePath("storybook-addon-rslib"),
+      name: getAbsolutePath('storybook-addon-rslib'),
       options: {
         rslib: {
-          include: ["**/stories/**"],
+          include: ['**/stories/**'],
         },
       },
     },
     {
-      name: "@module-federation/storybook-addon/preset",
+      name: '@module-federation/storybook-addon/preset',
       options: {
         // add your rslib module manifest here for storybook dev
         // we have set dev.assetPrefix and server.port to 3001 in rslib.config.ts above
@@ -46,13 +48,13 @@ const config: StorybookConfig = {
             // you can also add shared here for storybook app
             // shared: {}
             //  process.env.NODE_ENV
-            `react_islands@${reactIslandsUrl + "/mf-manifest.json"}`,
+            `react_islands@${reactIslandsUrl + '/mf-manifest.json'}`,
         },
       },
     },
   ],
   typescript: {
-    reactDocgen: "react-docgen-typescript",
+    reactDocgen: 'react-docgen-typescript',
   },
 };
 
