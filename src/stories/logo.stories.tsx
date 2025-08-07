41e9b83149ef5eaf6a48b19f710b4c7571eb80b4
Taytay <taylancetin06@gmail.com>
2025-08-07 23:16:27 +0300

    dark-light logo

--- src/stories/logo.stories.tsx
+++ src/stories/logo.stories.tsx
@@ -1,18 +1,33 @@
 import '@/styles.css';
 import Logo from 'react_islands/logo';
+import { Meta, StoryObj } from 'storybook-react-rsbuild';
 
-const Component = () => {
+const Component = ({ theme }: { theme: 'light' | 'dark' }) => {
   return (
-    <div className="w-full">
-      <Logo className="size-10 fill-blue-800" />
+    <div
+      className={`w-full ${theme === 'light' ? '' : 'bg-gray-800 text-white'} p-4`}
+    >
+      <Logo
+        className={`size-10 ${theme === 'light' ? 'fill-blue-800' : 'fill-blue-200'}`}
+      />
       Dershanı logosu
     </div>
   );
 };
 
-export default {
+const meta = {
   title: 'Dershani Logo',
   component: Component,
+} as Meta<typeof Component>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+  args: { theme: 'light' },
 };
 
-export const Primary = {};
+export const Dark: Story = {
+  args: { theme: 'dark' },
+};
